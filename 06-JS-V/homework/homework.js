b5c51f4301cf5e2b4f83655a12e4df984c93f09b
sofia-md <soffi.dominguez.2002@gmail.com>
2022-04-09 21:14:29 -0300

    Homework JSVI

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -67,13 +67,6 @@ class Persona{
   }
 }
 
-
-
-
-
-
-
-
 function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
